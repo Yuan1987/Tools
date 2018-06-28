362e8b3f8758771896096beda650022a1392c5a6
yuanhangbiao <yuanhangbiao@163.com>
2018-06-28 14:15:48 +0800

    1

--- src/main/java/com/yuan/controller/UploadController.java
+++ src/main/java/com/yuan/controller/UploadController.java
@@ -27,76 +27,64 @@ public class UploadController {
 
     private final static String excel2003L = ".xls"; // 2003- 版本的excel
     private final static String excel2007U = ".xlsx"; // 2007+ 版本的excel
-    
 
     @ApiOperation(value = "excelToJson")
     @PostMapping("/excelToJson")
-    public Map<String,List<Map<String, String>>> excelToJson(MultipartFile file) throws Exception {
-        
-        Map<String,List<Map<String, String>>> data = null;
-        
+    public Map<String, List<Map<String, String>>> excelToJson(MultipartFile file) throws Exception {
+
+        Map<String, List<Map<String, String>>> data = null;
+
         String filename = file.getOriginalFilename();
-        
-        if(file == null || (!filename.endsWith(excel2007U) && !filename.endsWith(excel2003L))) {
+
+        if (file == null || (!filename.endsWith(excel2007U) && !filename.endsWith(excel2003L))) {
             return data;
         }
-        
-        Map<String,List<List<String>>> sheetMap = null;
-        try(InputStream in = file.getInputStream()){
-            
+
+        Map<String, List<List<String>>> sheetMap = null;
+        try (InputStream in = file.getInputStream()) {
+
             sheetMap = this.getBankListByExcel(in, file.getOriginalFilename());
 
             data = new LinkedHashMap<>();
-            
+
             Iterator<Entry<String, List<List<String>>>> sheetIt = sheetMap.entrySet().iterator();
-            
-            while(sheetIt.hasNext()) {
+
+            while (sheetIt.hasNext()) {
                 Entry<String, List<List<String>>> en = sheetIt.next();
-                
+
                 List<List<String>> listob = en.getValue();
-                
-                List<Map<String, String>> sheetList =new ArrayList<>();
-                
+
+                List<Map<String, String>> sheetList = new ArrayList<>();
+
                 for (int i = 1; i < listob.size(); i++) {
-                    
+
                     Map<String, String> map = new LinkedHashMap<>();
 
-                    try {
-                        List<String> lo = listob.get(0);
+                    List<String> first = listob.get(0);
 
-                        for (String key : lo) {
-                            map.put(key.trim(), "");
-                        }
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    for (String key : first) {
+                        map.put(key.trim(), "");
                     }
 
-                    try {
-                        List<String> lo = listob.get(i);
-                        
-                        Iterator<String> it= map.keySet().iterator();
-                        
-                        for (String val : lo) {
-                            
-                            String key =it.next();
-                            
-                            System.out.println(key +"==" +val);
-                            map.put(key, val);
-                        }
-                        
-                        sheetList.add(map);
+                    List<String> lo = listob.get(i);
 
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    Iterator<String> it = map.keySet().iterator();
+
+                    for (String val : lo) {
+
+                        String key = it.next();
+
+                        map.put(key, val);
                     }
+
+                    sheetList.add(map);
                 }
                 data.put(en.getKey(), sheetList);
             }
         }
         return data;
     }
-    
+
     /**
      * 描述：根据文件后缀，自适应上传文件的版本
      * 
@@ -117,10 +105,9 @@ public class UploadController {
         return wb;
     }
 
-    public Map<String,List<List<String>>> getBankListByExcel(InputStream in, String fileName) throws Exception {
-        
-        
-        Map<String,List<List<String>>> sheetMap =new LinkedHashMap<>(16);
+    public Map<String, List<List<String>>> getBankListByExcel(InputStream in, String fileName) throws Exception {
+
+        Map<String, List<List<String>>> sheetMap = new LinkedHashMap<>(16);
 
         // 创建Excel工作薄
         Workbook work = this.getWorkbook(in, fileName);
@@ -130,19 +117,19 @@ public class UploadController {
         Sheet sheet = null;
         Row row = null;
         Cell cell = null;
-        
+
         for (int i = 0; i < work.getNumberOfSheets(); i++) {
             sheet = work.getSheetAt(i);
             if (sheet == null) {
                 continue;
             }
-            
+
             List<List<String>> list = new ArrayList<>();
-            
+
             for (int j = sheet.getFirstRowNum(); j < sheet.getLastRowNum() + 1; j++) {
-                
+
                 row = sheet.getRow(j);
-                if (row == null /*|| row.getFirstCellNum() == j*/) {
+                if (row == null /* || row.getFirstCellNum() == j */) {
                     continue;
                 }
 
@@ -150,10 +137,10 @@ public class UploadController {
                 for (int y = row.getFirstCellNum(); y < row.getLastCellNum(); y++) {
                     cell = row.getCell(y);
                     cell.setCellType(Cell.CELL_TYPE_STRING);
-                    if(cell != null && cell.getCellType()!=Cell.CELL_TYPE_BLANK){//空白判定
+                    if (cell != null && cell.getCellType() != Cell.CELL_TYPE_BLANK) {// 空白判定
                         li.add(cell.getStringCellValue());
                     }
-                    
+
                 }
                 list.add(li);
             }
